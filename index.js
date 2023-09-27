f9c0ea726592c4d458256a67a30591756c439398
Victor <victorprivv101@gmail.com>
2023-09-27 07:39:08 +0300

    copleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const textParagraph = document.querySelector('p');
+  
+    
+    if (textParagraph) {
+      
+      textParagraph.textContent = "This is really cool!";
+    }
+  });
+  
+  
